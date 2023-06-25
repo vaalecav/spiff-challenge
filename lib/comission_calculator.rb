3213491f9f4fb674acee10166041f3759bfb05f8
vaalecav <valit28@gmail.com>
2023-06-25 20:23:37 -0300

    Add empty class and spec

---
+++ lib/comission_calculator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# CommissionCalculator class
+class CommissionCalculator
+  # TODO: Complete ComissionCalculator
+end
+
+# Usage
+# calculator = CommissionCalculator.new
---
+++ spec/lib/comission_calculator_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'comission_calculator'
+
+RSpec.describe CommissionCalculator do
+  subject { ComissionCalculator.new }
+
+  describe '#method' do
+    it 'provide expectation' do
+      # TODO: Write ComissionCalculator specs
+    end
+  end
+end
